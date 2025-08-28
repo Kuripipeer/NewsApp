910626912a495262c3c2b608cef098c9ad6c2637
Kuripipeer <mchristianaxel@gmail.com>
2025-08-28 16:21:14 -0600

    Initial commit

---
+++ app/src/main/java/com/example/newsapp/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.example.newsapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.newsapp.ui.theme.NewsAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            NewsAppTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    HomeScreen(
+                        innerPadding
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun HomeScreen(innerPadding : PaddingValues){
+    Column (
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(innerPadding)
+            .padding(20.dp)
+    ) {
+
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    NewsAppTheme {
+        HomeScreen(
+            innerPadding = PaddingValues(0.dp)
+        )
+    }
+}
